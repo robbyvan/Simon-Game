9ae27d5d79b7495ed985ec39583dbdf87860e797
robbyvan <fzpinus@gmail.com>
2016-11-11 12:12:47 -0800

    1st version with no sound

--- simongame.js
+++ simongame.js
@@ -4,69 +4,91 @@
 按钮3: Strict, 在前开关打开的前提下监听, 如果打开, LED亮(开), 判断结果错误时进入另一个函数
 4个bar: 在开关和start的前提下, 且在用户输入阶段监听
 */
-/*button enable variable*/
-var power = false;
 
 /*game parameters*/
-var gameStatus = {};
-gameStatus.strict = false;
-gameStatus.stage = 0;
-gameStatus.sequence = [];
-gameStatus.player = [];
-gameStatus.events = [];
+var gameStatus = {
+  power: false,
+  strict: false,
+  lock: true,
+  stage: 0,
+  sequence: [],
+  player: [],
+  events: []
+};
 
 $(document).ready(function(){
   $('.switch').on('click', function(){
-    power = !power;
-    //在html和css中默认处于关闭, 若开启电源, 添加开启class
-    if (power) {
-      //电源槽滑动
-      gameInit();
-      $('.switch-btn').addClass('light');
-      //计数灯亮
-      $('.point h1').addClass('light');
-      //开灯
-      $('.start-btn').addClass('light');
-      //允许点击start和strict按钮, 即, 允许监听事件[可能要改鼠标效果]
-      $('.start-btn').on('click', startAllow);
-      $('.strict-btn').on('click', strictAllow);
-      $('.bar').mousedown('click', function(){
-        if (!gameStatus.lock) {
-          pushColor($(this));
-        }
-      });
-      $('.bar').mouseup(function(e){
-        e.stopPropagation(e);
-        if (!gameStatus.lock) {
-          $('.bar').removeClass('light');
-        }
-      });
+    gameStatus.power = !gameStatus.power;
+
+    if (gameStatus.power) {
+      powerOn();
     }else {
-      //电源槽归位
-      resetTimers();
-      $('.switch-btn').removeClass('light');
-      //计数灯灭
-      $('.point h1').removeClass('light');
-      //关灯
-      $('.start-btn').removeClass('light');
-      $('.strict-btn').removeClass('light');
-      //解除start和strict的事件监听[可能要改鼠标效果]
-      $('.start-btn').off('click', startAllow);
-      $('.strict-btn').off('click', strictAllow);
-      gameStatus.lock = true;
-      // $('.bar').off('click', judge);
+      powerOff();
     }
+
   });
 });
 
-function startAllow(){
-  console.log("Allow click start.");
+function gameInit(){
+  resetTimers(); // reset all timers
+  gameStatus.sequence.length = 0; //reset the sequence
+  gameStatus.player.length = 0; //reset the player's sequence
+  gameStatus.stage = 0; //back to inital stage
+  $('.point h1').html('--');//back to inital stage
+  gameStatus.lock = true; //don't allow click bars
+  $('.bar').removeClass('clickable');//don't allow click bars
+}
+
+function powerOn(){
   gameInit();
+  $('.switch-btn').addClass('swtichOn');
+  //计数灯亮
+  $('.point h1').addClass('light');
+  //按钮开灯
+  $('.start-btn').addClass('light').addClass('clickable');
+  $('.strict-btn').addClass('clickable');
+  //允许点击start和strict按钮
+  $('.start-btn').on('click', startAllow);
+  $('.strict-btn').on('click', strictAllow);
+  //监控bar点击事件
+  $('.bar').mousedown('click', function(){
+    if (!gameStatus.lock) {
+      pushColor($(this));
+    }
+  });
+  $('.bar').mouseup(function(e){
+    e.stopPropagation(e);
+    if (!gameStatus.lock) {
+      $('.bar').removeClass('light');
+    }
+  });  
+}
+
+function powerOff(){
+  //清除所有定时器
+  resetTimers();
+  //电源槽归位
+  $('.switch-btn').removeClass('swtichOn');
+  //计数灯灭, 显示'--'
+  $('.point h1').removeClass('light').html('--');
+  //bar不可点击
+  gameStatus.lock = true;
+  $('.bar').removeClass('clickable');
+  //关按钮灯, 解除鼠标clickable效果
+  $('.start-btn').removeClass('light').removeClass('clickable');
+  $('.strict-btn').removeClass('light').removeClass('clickable');
+  //解除对start和strict的监听
+  $('.start-btn').off('click', startAllow);
+  $('.strict-btn').off('click', strictAllow);
+}
+
+function startAllow(){
+  console.log("player clicked start.");
   gameRun();
 }
 
 function strictAllow(){
-  console.log("Allow click strict.");
+  console.log("player clicked strict.");
   gameStatus.strict = !gameStatus.strict;
   if (gameStatus.strict) {
     $('.strict-btn').addClass('light');
@@ -75,96 +97,109 @@ function strictAllow(){
   }
 }
 
-function gameInit(){
-  gameStatus.sequence.length = 0; //reset the squence
-  gameStatus.events.length = 0;
-  gameStatus.stage = 0;
-  gameStatus.player = [];
-  gameStatus.lock = true;
-  $('.point h1').html('--');
-}
-
 function gameRun(){
-  //按照顺序执行
-  // gameStatus.stageSeq = setInterval(function(){
-    //总循环, 每关步骤
-    gameStatus.stage += 1;
-    // for (var i = 0; i < 4; i++){
-    //产生一个新颜色
+  /*执行一关的流程: 关卡更新 -> 显示颜色序列 -> 等待输入*/
+
+  /*按对了进来时有一个timeout gameStatus.evt*/
+
+  //设定样式
+    gameStatus.lock = true;//显示时不允许点击bar
+    $('.bar').removeClass('clickable');//显示时不允许点击bar
+
+  //关卡+1, 产生一个新颜色
+    gameStatus.stage += 1;//关+1
     var newColor = Math.floor(4 * Math.random());
     gameStatus.sequence.push(newColor);
 
-    //显示颜色顺序
-    showColors();
+  //显示颜色并等待
+    gameStatus.evt = setTimeout(showColors, 1000);//在开始后, 延迟1s开始显示颜色, userfriendly
 }
 
 function showColors(){
+  resetTimers();//清除gameRun & restart的gameStatus.evt计时器
+  /*计分板更新*/
+  $('.point h1').addClass('light');//确保计分板灯亮, 后面错误闪烁可能意外关灯
+  $('.point h1').html(gameStatus.stage);//计分板数字更新
 
-  $('.point h1').html(gameStatus.stage);
-  gameStatus.player.length = 0;//初始化新一关
-  var index = 0;
-  if (index < gameStatus.sequence.length) {
-    gameStatus.lightOn = setInterval(function(){
-      //当前颜色
-      var currColor = gameStatus.sequence[index];
-      //每关步骤第一步, 遍历显示所有灯
-      index++;//显示了一个灯,计数+1
-      console.log("displaying " + index + "st light");
-      $('#' + currColor).addClass('light');
-      //等待一段时间,2s,关灯
-      gameStatus.lightOff = setTimeout(function(){
-        $('#' + currColor).removeClass('light');
-        console.log("turning off the light");
-      }, 500);
-      if (index >= gameStatus.sequence.length){
-        //如果显示到最后一个灯, 结束循环
-        clearInterval(gameStatus.lightOn);
-        //打开点击权限
-        gameStatus.playerIndex = -1;
-        gameStatus.lock = false;
-        gameStatus.evt = setTimeout(errReport, 5000);//五秒不按, 失败
-      }
-    }, 1000);
-  }
+  /*开始显示*/
+  gameStatus.player.length = 0; //有可能是非strict重新挑战, 清零player
+  var index = 0;//index表示当前显示颜色角标
+  // if (index < gameStatus.sequence.length) { //应该可以去掉第一次判断, 因为来到这里一定至少有一个颜色
+  
+  /*循环显示颜色, 1000ms一次*/
+  gameStatus.lightOn = setInterval(function(){
+    var currColor = gameStatus.sequence[index];//当前颜色
+    index++;//显示了一个灯,计数+1
+    console.log("displaying " + index + "st light");
+
+    /*light + sound, 亮灯操作, 目前缺少音效*/
+    $('#' + currColor).addClass('light');
+    /*保持light+sound一段时间(500ms), 然后关灯关声音*/
+    gameStatus.lightOff = setTimeout(function(){
+      /*关灯关声音*/
+      $('#' + currColor).removeClass('light');
+      console.log("turning off the light");
+    }, 500);
+
+    /*在每次显示一个灯后, 确认当前序列显示是否完成*/
+    if (index >= gameStatus.sequence.length){
+      /*结束循环, 关lightOn定时器*/
+      clearInterval(gameStatus.lightOn);
+
+      gameStatus.playerIndex = -1;//初始化playerIndex
+
+      /*打开点击权限lock = false, bar监听中允许判断用户输入*/
+      gameStatus.lock = false;
+      $('.bar').addClass('clickable');
+
+      /*显示完灯后开始计时, 如果t=5000ms秒内用户不按, 直接失败*/
+      gameStatus.evt = setTimeout(errReport, 5000);
+    }
+  }, 1000);
+  // }end if
 }
 
 function pushColor(pushedBar){
-  // if (!gameStatus.lock) {
-    clearTimeout(gameStatus.evt);
-    gameStatus.playerIndex += 1;
-    pushedBar.addClass('light');
-    gameStatus.player.push(pushedBar.attr('id'));
-    var currColor = gameStatus.player[gameStatus.playerIndex];
+  /*每当按了一个bar, 就清除timeout timer*/
+  clearTimeout(gameStatus.evt);
 
-    if (currColor == gameStatus.sequence[gameStatus.playerIndex]) {
-      //按对了
-      if (gameStatus.playerIndex < (gameStatus.sequence.length-1) ) {
-        //还没按完, 继续按
+  /*用户已按数目+1*/
+  gameStatus.playerIndex += 1;
+  pushedBar.addClass('light');//??????只开灯, 因为不知flasherr最终状态
+
+  /*保存用户输入? 貌似没必要存用户所有输入?*/
+  gameStatus.player.push(pushedBar.attr('id'));
+  var currColor = gameStatus.player[gameStatus.playerIndex];//可以直接取点击bar的id
+
+  if (currColor == gameStatus.sequence[gameStatus.playerIndex]) {
+    //按对了
+    if (gameStatus.playerIndex < (gameStatus.sequence.length-1) ) {
+      //但还没按完, 要继续按, 只需重新建立timeout timer
         gameStatus.evt = setTimeout(errReport, 5000);//五秒不按, 失败
-      }else{
-        //按完了
-        // $('.bar').unbind('click', pushColor);
-        // $('.bar').off('mouseup', releasePushing);//关掉点击权限
-        gameRun();//开始下一关
-      }
+    }else{
+      //按完了, 延时1s进入下一关
+      gameStatus.evt = setTimeout(gameRun, 1000);//开始下一关
+    }
     }else {
+      //按错了
       errReport();
     }
 }
 function errReport(){
-  flashErrMsg();
+  flashErrMsg(2);
+
+  /*提示错误flashErr: 2000ms*/
   gameStatus.evt = setTimeout(function(){
     if (gameStatus.strict) {
       console.log('you lose!');
-      resetTimers();
       gameInit();
-      gameRun();
+      gameStatus.evt = setTimeout(gameRun, 2000);
     }else{
       console.log('try again!');
       resetTimers();
-      showColors();
+      gameStatus.evt = setTimeout(showColors, 2000);
     }
-  }, 2000);//提示错误2秒后开始
+  }, 2000);
 }
 
 function resetTimers(){
@@ -175,19 +210,30 @@ function resetTimers(){
   clearInterval(gameStatus.flashOn);
 }
 
-function flashErrMsg(msg, duration){
-  resetTimers();
+function flashErrMsg(times){
+  //没按完进来时, 有: 5s的timeout timer .evt
+  resetTimers();//已经错了, 清除time out计时, 有可能第一个按错, 当时还有一个关灯计时timeout
+
+  /*感叹号表示错了*/
+  //可以关一次灯?
+  $('.point h1').removeClass('light');
   $('.point h1').html('!!');
+
+  /*同颜色显示流程, count记录闪烁次数, 一共闪times次*/
   var count = 0;
+  /*延时t1 = 800ms后开计数板灯*/
   gameStatus.flashOn = setInterval(function(){
     $('.point h1').addClass('light');
+    //缺少声音
+    /*保持灯亮t2 = 400ms后, 关灯*/
     gameStatus.flashOff = setTimeout(function(){
       $('.point h1').removeClass('light');
-      count += 1;
-      if (count >= duration) {
+      count += 1;//关灯计数?? 开灯计数??
+    }, 250);
+    /*每次循环后, 查看是否闪烁够了次数*/
+    if (count >= times) {
         clearTimeout(gameStatus.flashOff);
         clearInterval(gameStatus.flashOn);
       }
-    }, 300);
   }, 500);
 }
