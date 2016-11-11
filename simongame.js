9ba58ca7cd08edc801e95ded56858101cb4b1e2b
robbyvan <fzpinus@gmail.com>
2016-11-11 00:39:20 -0800

    finished showColor & player input

--- simongame.js
+++ simongame.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
     //在html和css中默认处于关闭, 若开启电源, 添加开启class
     if (power) {
       //电源槽滑动
+      gameInit();
       $('.switch-btn').addClass('light');
       //计数灯亮
       $('.point h1').addClass('light');
@@ -29,9 +30,21 @@ $(document).ready(function(){
       //允许点击start和strict按钮, 即, 允许监听事件[可能要改鼠标效果]
       $('.start-btn').on('click', startAllow);
       $('.strict-btn').on('click', strictAllow);
+      $('.bar').mousedown('click', function(){
+        if (!gameStatus.lock) {
+          pushColor($(this));
+        }
+      });
+      $('.bar').mouseup(function(e){
+        e.stopPropagation(e);
+        if (!gameStatus.lock) {
+          $('.bar').removeClass('light');
+        }
+      });
     }else {
       //电源槽归位
-      $('switch-btn').removeClass('light');
+      clearInterval(gameStatus.lightOn);
+      $('.switch-btn').removeClass('light');
       //计数灯灭
       $('.point h1').removeClass('light');
       //关灯
@@ -40,7 +53,8 @@ $(document).ready(function(){
       //解除start和strict的事件监听[可能要改鼠标效果]
       $('.start-btn').off('click', startAllow);
       $('.strict-btn').off('click', strictAllow);
-      $('.bar').off('click', judge);
+      gameStatus.lock = true;
+      // $('.bar').off('click', judge);
     }
   });
 });
@@ -65,6 +79,9 @@ function gameInit(){
   gameStatus.sequence.length = 0; //reset the squence
   gameStatus.events.length = 0;
   gameStatus.stage = 0;
+  gameStatus.player = [];
+  gameStatus.lock = true;
+  $('.point h1').html('--');
 }
 
 function gameRun(){
@@ -72,6 +89,7 @@ function gameRun(){
   // gameStatus.stageSeq = setInterval(function(){
     //总循环, 每关步骤
     //更新stage面板
+    // for (var i = 0; i < 4; i++){
     gameStatus.stage += 1;
     $('.point h1').html(gameStatus.stage);
 
@@ -81,20 +99,12 @@ function gameRun(){
 
     //显示颜色顺序
     showColors();
-    
-    //玩家输入
-    // playersTurn();
-
-    //判断
-    judge();
-
-  // }, 3000);
 }
 
 function showColors(){
+  gameStatus.player.length = 0;//初始化新一关
   var index = 0;
   if (index < gameStatus.sequence.length) {
-    
     gameStatus.lightOn = setInterval(function(){
       //当前颜色
       var currColor = gameStatus.sequence[index];
@@ -107,13 +117,50 @@ function showColors(){
         $('#' + currColor).removeClass('light');
         console.log("turning off the light");
       }, 500);
-      if (index === gameStatus.sequence.length){
+      if (index >= gameStatus.sequence.length){
         //如果显示到最后一个灯, 结束循环
         clearInterval(gameStatus.lightOn);
         //打开点击权限
-        $('.bar').on('click', judge);
+        gameStatus.playerIndex = -1;
+        gameStatus.lock = false;
       }
     }, 1000);
   }
 }
 
+function pushColor(pushedBar){
+  // if (!gameStatus.lock) {
+    clearTimeout(gameStatus.wait);
+    gameStatus.playerIndex += 1;
+    pushedBar.addClass('light');
+    gameStatus.player.push(pushedBar.attr('id'));
+    var currColor = gameStatus.player[gameStatus.playerIndex];
+
+    if (currColor == gameStatus.sequence[gameStatus.playerIndex]) {
+      //按对了
+      if (gameStatus.playerIndex < (gameStatus.sequence.length-1) ) {
+        //还没按完, 继续按
+        gameStatus.wait = setTimeout(loseGame, 5000);//五秒不按, 失败
+      }else{
+        //按完了
+        // $('.bar').unbind('click', pushColor);
+        // $('.bar').off('mouseup', releasePushing);//关掉点击权限
+        gameRun();//开始下一关
+      }
+    }else {
+      //按错了, 检查strict否
+      if (gameStatus.strict) {
+        //errReport();
+        loseGame();//重头再来
+      }else{
+        //提示一次
+        //errReport();
+        showColors();
+      }
+    }
+  // }
+}
+
+function loseGame(){
+  console.log("you lose!");
+}
